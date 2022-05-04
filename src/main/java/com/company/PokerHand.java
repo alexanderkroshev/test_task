6f8cc7a0418a42f978d32939ce243c81c47bab6b
alexanderkroshev <alexanderkroshev@gmail.com>
2022-05-04 23:52:50 +0700

    some changes

--- src/main/java/com/company/PokerHand.java
+++ src/main/java/com/company/PokerHand.java
@@ -3,7 +3,8 @@ package com.company;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-import java.util.*;
+import java.util.List;
+import java.util.Set;
 
 @Getter
 @RequiredArgsConstructor
@@ -19,71 +20,18 @@ public class PokerHand implements Comparable<PokerHand> {
         return SuitCardParser.getSuitCardSet(cards);
     }
 
-    private int secondCriteria() {
-        Combination combination = getCombination();
-        if (combination == Combination.HIGH_CARD)
-            return getCardPowerByFrequency(1);
-        if (combination == Combination.PAIR || combination == Combination.TWO_PAIRS)
-            return getCardPowerByFrequency(2);
-        if (combination == Combination.THREE_OF_A_KIND)
-            return getCardPowerByFrequency(3);
-        if (combination == Combination.FOUR_OF_A_KIND)
-            return getCardPowerByFrequency(4);
-        if (combination == Combination.FULL_HOUSE)
-            return getCardPowerByFrequency(3);
-        if (
-                combination == Combination.STRAIGHT
-                        || combination == Combination.FLUSH
-                        || combination == Combination.STRAIGHT_FLUSH
-        )
-            return getCardPowerByFrequency(1);
-        else
-            return 0;
-    }
-
-    private int thirdCriteria() {
-        Combination combination = getCombination();
-        if (combination == Combination.PAIR)
-            return getCardPowerByFrequency(1);
-        if (combination == Combination.TWO_PAIRS) {
-            List<Integer> cards = new ArrayList<>();
-            Map<Card, Integer> map = SuitCard.calcFrequencyMap(getSuitCardSet());
-            for ( Map.Entry<Card, Integer> i : map.entrySet() ) {
-                if (i.getValue() == 2)
-                    cards.add(i.getKey().getPower());
-            }
-            return Collections.min(cards);
-        } else
-            return 0;
-    }
-
-    private int fourthCriteria() {
-        if (getCombination() == Combination.TWO_PAIRS)
-            return getCardPowerByFrequency(1);
-        else
-            return 0;
-    }
-
     @Override
     public int compareTo(PokerHand o) {
-        int result;
-        result = o.getCombination().getPower() - this.getCombination().getPower();
-        if (result == 0)
-            result = o.secondCriteria() - this.secondCriteria();
-        if (result == 0)
-            result = o.thirdCriteria() - this.thirdCriteria();
-        if (result == 0)
-            result = o.fourthCriteria() - this.fourthCriteria();
-        return result;
-    }
-
-    private int getCardPowerByFrequency(int frequency) {
-        List<Integer> cards = new ArrayList<>();
-        Map<Card, Integer> map = SuitCard.calcFrequencyMap(getSuitCardSet());
-        for ( Map.Entry<Card, Integer> i : map.entrySet() ) {
-            if (i.getValue() == frequency)
-                cards.add(i.getKey().getPower());
+        CombinationCriteria thisCriteria = new CombinationCriteria(this.getSuitCardSet());
+        CombinationCriteria oCriteria = new CombinationCriteria(o.getSuitCardSet());
+        List<Integer> thisList = thisCriteria.criteriaList();
+        List<Integer> oList = oCriteria.criteriaList();
+        int result = 0;
+        for ( int i = 0; i < thisList.size(); i++ ) {
+            result = oList.get(i) - thisList.get(i);
+            if (result != 0)
+                break;
         }
-        return Collections.max(cards);
+        return result;
     }
 }
